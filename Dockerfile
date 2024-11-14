61e4ca3ed34e8c3f8d2c0e89ef99785ffd9ff6c8
Nathaly Victoriano <nathalyvictorianodiaz2020fb@gmail.com>
2024-11-14 12:58:48 -0300

    I added a Dockerfile and dockercompose

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:18.20-alpine3.20
+
+RUN mkdir -p /home/app
+COPY . /home/app
+EXPOSE 3000
+CMD [ "npm", "start" ]
+
--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 
 function App  () {
   const [count, setCount]  = useState(0)
-const j = 0;
   return (
     <>
       <div>
