6b5ba46c54adafdd0c1443494e7ab7f391b9bfd5
Intellisys <intellisys@MacBook-Pro-de-Intellisys.local>
2025-03-18 10:00:51 -0400

    Finally url protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
+
 import { Login } from '@layout/Login';
 import { Homepage } from '@components/Homepage'
-import { Routes, Route } from 'react-router'
-import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react'
-
-
+import { SignedOut, SignedIn } from '@clerk/clerk-react'
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="" element={<Login />} />
-        <Route path='Homepage' element={
-          <SignedIn>
-            <Homepage />
-          </SignedIn>
-        } />
-        <Route path='/Homepage' element={
-          <SignedOut>
-            <Login/>
-          </SignedOut>
-        } />
-      </Routes>
-    </>
+      <SignedOut>
+        <Login />
+      </SignedOut>
 
+      <SignedIn>
+        <Homepage />
+      </SignedIn>
+    </>
   )
 }
 
