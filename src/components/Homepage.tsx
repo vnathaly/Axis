62cb0ddc4fb0688dd4d88cddf200201ff7ddaf4b
Nathaly <nathalyvictorian7@gmail.com>
2025-06-22 19:48:28 -0400

    Cloning

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,16 +1,11 @@
-import { Footer } from '@layout/Footer';
 import { Navbar } from '@layout/Navbar';
-import { Carousel } from '@layout/carousel'
+
 
 export function Homepage() {
 
         return (
             <>
                 <Navbar />
-                <Carousel />
-                <Carousel />
-                <Carousel />
-                <Footer />
             </>
         )
 }
